6bc4d49b796cb6baa9ea7844c56646c9eb446ad2
Drew <drewsup123c@gmail.com>
2019-08-26 15:46:12 -0600

    added some icons to tabnavigator

--- instagram/navigation/TabNavigator.js
+++ instagram/navigation/TabNavigator.js
@@ -6,11 +6,32 @@ import Activity from '../screens/Activity.js';
 import Profile from '../screens/Profile.js';
 import Search from '../screens/Search';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// Expo Icons
+import {Ionicons, Feather} from '@expo/vector-icons';
 
 const TabNavigator = createBottomTabNavigator({
-    Home : Home,
-    Activity : Activity,
-    Profile : Profile,
+    Home : {
+        screen : Home,
+        navigationOptions : {
+            tabBarLabel : '',
+            tabBarIcon : () => <Ionicons name="ios-home" color="green" size={35}/>
+        }
+    },
+    Activity :{ 
+        screen : Activity,
+        navigationOptions : {
+            tabBarLabel : '',
+            tabBarIcon : () => <Feather name="activity" color="green" size={35} />
+        }
+    },
+    // Upload : '',
+    Profile : {
+        screen : Profile,
+        navigationOptions : {
+            tabBarLabel : '',
+            tabBarIcon : () => <Feather name="profile" color="green" size={35} />
+        }
+    },
     Search : Search
 })
 
