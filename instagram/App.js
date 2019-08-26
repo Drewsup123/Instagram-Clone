8b4268c4ff78633c5cbd914966dbd6bced8efbfe
Drew <drewsup123c@gmail.com>
2019-08-26 00:22:19 -0600

    added react-navigation, and imported screens to app.js

--- instagram/App.js
+++ instagram/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+//Screen Imports
+import Home from './screens/Home.js';
+import Activity from './screens/Activity.js';
+import Profile from './screens/Profile.js';
+import Search from './screens/Search';
 
 export default function App() {
   return (
