767f0863026e7f57d6ecb88ad74ba64de0b48aff
Drew <drewsup123c@gmail.com>
2019-08-26 22:43:52 -0600

    created focused styles for navigation

--- instagram/navigation/TabNavigator.js
+++ instagram/navigation/TabNavigator.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Home from '../screens/Home.js';
 import Activity from '../screens/Activity.js';
 import Profile from '../screens/Profile.js';
-import Search from '../screens/Search';
+import Search from '../screens/Search.js';
+import Upload from '../screens/Upload.js';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 // Expo Icons
 import {Ionicons, Feather} from '@expo/vector-icons';
@@ -13,26 +14,39 @@ const TabNavigator = createBottomTabNavigator({
     Home : {
         screen : Home,
         navigationOptions : {
-            tabBarLabel : '',
-            tabBarIcon : () => <Ionicons name="ios-home" color="green" size={35}/>
+            tabBarLabel : ' ',
+            tabBarIcon : ({focused}) => <Ionicons name="ios-home" color={focused ? "blue" : 'black'} size={35}/>
+        }
+    },
+    Search : {
+        screen : Search,
+        navigationOptions : {
+            tabBarLabel : ' ',
+            tabBarIcon : ({focused}) => <Ionicons name="ios-search" color={focused ? "blue" : 'black'} size={35} />
+        }
+    },
+    Upload : {
+        screen : Upload,
+        navigationOptions : {
+            tabBarLabel : ' ',
+            tabBarIcon : ({focused}) => <Ionicons name="ios-add-circle-outline" color={focused ? "blue" : 'black'} size={35} />
         }
     },
     Activity :{ 
         screen : Activity,
         navigationOptions : {
-            tabBarLabel : '',
-            tabBarIcon : () => <Feather name="activity" color="green" size={35} />
+            tabBarLabel : ' ',
+            tabBarIcon : ({focused}) => <Ionicons name="ios-heart" color={focused ? "blue" : 'black'} size={35} />
         }
     },
     // Upload : '',
     Profile : {
         screen : Profile,
         navigationOptions : {
-            tabBarLabel : '',
-            tabBarIcon : () => <Feather name="profile" color="green" size={35} />
+            tabBarLabel : ' ',
+            tabBarIcon : ({focused}) => <Ionicons name="ios-person" color={focused ? "blue" : 'black'} size={35} />
         }
     },
-    Search : Search
 })
 
 export default createAppContainer(TabNavigator);
